bd354b33baee65f8b9a7acccc6d6eaf1d741498f
MohamedOsama1804 <146191989+MohamedOsama1804@users.noreply.github.com>
2026-09-25 14:55:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'courier-directory-v3';
+const CACHE_NAME = 'courier-directory-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './xlsx.full.min.js'];
 
 self.addEventListener('install', (event) => {
